9efd88995fcfa3c1c607f297cef395e14ac66f50
Daniel Martínez <dannzm22@gmail.com>
2026-02-04 15:08:31 -0400

    Remove i18n

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
     formats: ['image/avif', 'image/webp'],
   },
-  i18n: {
-    locales: ['es', 'en'],
-    defaultLocale: 'es',
-    localeDetection: false,
-  },
 }
 
 module.exports = nextConfig
